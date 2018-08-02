dba9a2b7e6947fd6fe2ebd765df146746022f29d
root <anderson_fos@hotmail.com>
2018-08-02 12:33:04 -0300

    push inicial

---
+++ js/push.js
@@ -0,0 +1,16 @@
+console.log('[Application] start push listening');
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission().then(function() {
+    console.log('Permission granted');
+    
+    return messaging.getToken().then(function(currentToken) {
+        if(currentToken) {
+            console.log(currentToken);
+            return currentToken;
+        } else {
+            console.warn('Nenhum id disponível, solicite permissão para gerar um');
+        }
+    });
+});
